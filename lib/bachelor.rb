e38d4aacb24b6954e1b0b0e54ba6ae61c1337117
Eyal Toledano <eyaltoledano@me.com>
2019-02-14 03:47:54 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -49,7 +49,7 @@ def get_average_age_for_season(data, season)
   data.each do |show_season, contestants|
     if show_season == season
       contestants.each do |contestant|
-        contestant_ages << contestant["age"]
+        contestant_ages << contestant["age"].to_f
       end
     end
   end
