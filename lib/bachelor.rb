eaadf44d29f1509f8c86dd1012c5508daebcb532
Eyal Toledano <eyaltoledano@me.com>
2019-02-14 03:51:43 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -54,5 +54,5 @@ def get_average_age_for_season(data, season)
     end
   end
   age_sum = contestant_ages.inject(0) { |sum, i| sum + i }
-  average = age_sum / contestant_ages.size.round(0)
+  average = age_sum / contestant_ages.size.round(1)
 end
